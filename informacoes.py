1c04004efdf3d1eb020b49b9e88d66a53e683b2d
Claudio Bastos de Souza <claudiobastoss@hotmail.com>
2024-02-02 21:12:11 -0300

    Versao1

---
+++ informacoes.py
@@ -0,0 +1,10 @@
+nome = input('Digite seu nome ')
+print(nome)
+idade = int(input('Qual a sua idade '))
+print(idade)
+cidade = input('Qual sua Cidade ')
+print(cidade) 
+if idade >= 18:
+    print('Você está apto para o trabalho')
+else:
+    print('Somente maiores de idade ')    
